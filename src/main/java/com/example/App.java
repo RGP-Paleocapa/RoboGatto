6586fcefea36e5674aff14268787f4ab19aadb9f
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-05-23 14:43:55 +0200

    Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import com.example.components.MainPage;
+
+public class App {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Tom and Jerry");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        MainPage main = new MainPage(new BorderLayout());
+
+        window.add(main);
+
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
---
+++ src/main/java/com/example/entities/Entity.java
@@ -0,0 +1,116 @@
+package com.example.entities;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.example.components.PlayGround;
+
+public class Entity {
+	PlayGround pg;
+	public int worldX, worldY;
+	public int speed;
+
+	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	public String direction;
+
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
+	public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+	public int solidAreaX, solidAreaY;
+	public boolean collisionOn = false;
+	public int actionLockCounter = 0;
+
+	public Entity(PlayGround pg) {
+		this.pg = pg;
+	}
+
+	public void setAction() {
+
+	}
+
+	public void update() {
+		setAction();
+		collisionOn = false;
+		pg.cChecker.checkTile(this);
+		pg.cChecker.checkCat(this);
+		if (!collisionOn) {
+			switch (direction) {
+				case "up":
+					worldY -= speed;
+					break;
+				case "down":
+					worldY += speed;
+					break;
+				case "left":
+					worldX -= speed;
+					break;
+				case "right":
+					worldX += speed;
+					break;
+			}
+		}
+		spriteCounter++;
+		if (spriteCounter > 10) {
+			if (spriteNum == 1) {
+				spriteNum = 2;
+			} else if (spriteNum == 2) {
+				spriteNum = 1;
+			}
+			spriteCounter = 0;
+		}
+	}
+
+	public void draw(Graphics2D g2) {
+		BufferedImage image = null;
+
+		switch (direction) {
+			case "up":
+				if (spriteNum == 1) {
+					image = up1;
+				}
+				if (spriteNum == 2) {
+					image = up2;
+				}
+				break;
+			case "down":
+				if (spriteNum == 1) {
+					image = down1;
+				}
+				if (spriteNum == 2) {
+					image = down2;
+				}
+				break;
+			case "left":
+				if (spriteNum == 1) {
+					image = left1;
+				}
+				if (spriteNum == 2) {
+					image = left2;
+				}
+				break;
+			case "right":
+				if (spriteNum == 1) {
+					image = right1;
+				}
+				if (spriteNum == 2) {
+					image = right2;
+				}
+				break;
+		}
+		g2.drawImage(image, worldX, worldY, pg.tileSize, pg.tileSize, null);
+	}
+
+	public BufferedImage setup(String imagePath) {
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream(imagePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
+}
